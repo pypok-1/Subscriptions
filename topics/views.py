88dfa8ba4c236c33f0760bf206d81d541786b5e0
Eva <evacherkashyna9@gmail.com>
2025-12-25 16:39:57 +0200

    Refactor topic views and improve caching logic

--- topics/views.py
+++ topics/views.py
@@ -7,50 +7,46 @@ from .forms import TopicForm
 from .models import Topic, TopicActivity
 
 
-@login_required
 def topics_view(request: HttpRequest) -> HttpResponse:
-    topics_key = f"user:{request.user.id}:topics"
+    all_topics = Topic.objects.all()
+
+    topics_key = "all_topics_cache"
     data = cache.get(topics_key)
 
     if data is None:
-        data = request.user.topics.all()
+        data = Topic.objects.all()
         cache.set(topics_key, data, timeout=60)
 
-    all_topics = Topic.objects.all()
-    return render(request, "topics/topics.html",{
+    return render(request, "topics/topics.html", {
         "user_topics": data,
         "all_topics": all_topics,
     })
 
 
-@login_required
 def subscribe_view(request: HttpRequest, id: int) -> HttpResponse:
     topic = get_object_or_404(Topic, id=id)
-    topic.subscribers.add(request.user)
+    if request.user.is_authenticated:
+        topic.subscribers.add(request.user)
     return redirect("topics_list")
 
 
-@login_required
 def unsubscribe_view(request: HttpRequest, id: int) -> HttpResponse:
     topic = get_object_or_404(Topic, id=id)
-    topic.subscribers.remove(request.user)
+    if request.user.is_authenticated:
+        topic.subscribers.remove(request.user)
     return redirect("topics_view")
 
 
-@login_required
 def create_topic_view(request: HttpRequest) -> HttpResponse:
     if request.method == 'POST':
         form = TopicForm(request.POST)
         if form.is_valid():
             topic = form.save(commit=False)
-            topic.author = request.user
             topic.save()
         return redirect('topics_view')
 
 
-@login_required
 def delete_topic_view(request: HttpRequest, id:int) -> HttpResponse:
     topic = get_object_or_404(Topic, id=id)
-    if topic.author == request.user:
         topic.delete()
         return redirect('topics_view')
