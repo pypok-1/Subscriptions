b020113e21968e40b0a4249dd02ea5175b772c3e
Eva <evacherkashyna9@gmail.com>
2025-12-25 16:14:12 +0200

    Add delete and create topic views to urlpatterns

--- topics/urls.py
+++ topics/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('subscribe/<int:id>/', views.subscribe_view, name='subscribe_view'),
     path('', views.topics_view, name='topics_view'),
-    path("unsubscribe/<int:id>/", views.unsubscribe_view, name='unsubscribe_view')
+    path("unsubscribe/<int:id>/", views.unsubscribe_view, name='unsubscribe_view'),
+    path("delete/<int:id>/", views.delete_topic_view, name='delete_topic_view'),
+    path("create/", views.create_topic_view, name='create_topic_view'),
 ]
