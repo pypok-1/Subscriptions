2cb449932dbd3a3d1eecafe11de4ced9cde69bc1
Eva <evacherkashyna9@gmail.com>
2025-12-26 17:58:48 +0200

    Refactor topic views with user-specific caching
    Added caching per user and optimized topic retrieval.

--- topics/views.py
+++ topics/views.py
@@ -2,52 +2,63 @@ from django.contrib.auth.decorators import login_required
 from django.core.cache import cache
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
-
 from .forms import TopicForm
-from .models import Topic, TopicActivity
+from .models import Topic
 
 
+@login_required
 def topics_view(request: HttpRequest) -> HttpResponse:
-    all_topics = Topic.objects.all()
-
-    topics_key = "all_topics_cache"
+    ''' Відображає список усіх тем, використовуючи кешування для конкретного користувача для оптимізації запитів. '''
+    topics_key = f"user:{request.user.id}:topics"
     data = cache.get(topics_key)
 
     if data is None:
-        data = Topic.objects.all()
+        # views.py
+        data = Topic.objects.prefetch_related('subscribers').all()
         cache.set(topics_key, data, timeout=60)
 
     return render(request, "topics/topics.html", {
-        "user_topics": data,
-        "all_topics": all_topics,
+        "topics": data,
     })
 
 
-def subscribe_view(request: HttpRequest, id: int) -> HttpResponse:
-    topic = get_object_or_404(Topic, id=id)
-    if request.user.is_authenticated:
-        topic.subscribers.add(request.user)
-    return redirect("topics_list")
-
-
-def unsubscribe_view(request: HttpRequest, id: int) -> HttpResponse:
-    topic = get_object_or_404(Topic, id=id)
-    if request.user.is_authenticated:
-        topic.subscribers.remove(request.user)
-    return redirect("topics_view")
-
-
+@login_required
 def create_topic_view(request: HttpRequest) -> HttpResponse:
+    ''' Створює нову тему через форму, очищує загальний кеш тем після успішного збереження та перенаправляє на список. '''
     if request.method == 'POST':
         form = TopicForm(request.POST)
         if form.is_valid():
-            topic = form.save(commit=False)
-            topic.save()
-        return redirect('topics_view')
+            form.save()
+            cache.delete("all_topics_cache")
+            return redirect('topics_view')
+    else:
+        form = TopicForm()
+    return render(request, 'topics/create_topic.html', {'form': form})
 
 
-def delete_topic_view(request: HttpRequest, id:int) -> HttpResponse:
+@login_required
+def delete_topic_view(request: HttpRequest, id: int) -> HttpResponse:
+    ''' Видаляє тему за її ID та перенаправляє користувача до загального списку тем. '''
     topic = get_object_or_404(Topic, id=id)
     topic.delete()
     return redirect('topics_view')
 
+
+@login_required
+def subscribe_view(request: HttpRequest, id: int) -> HttpResponse:
+    ''' Підписує користувача на обрану тему, скидає загальний кеш та повертає до списку тем. '''
+    topic = get_object_or_404(Topic, id=id)
+    if request.user.is_authenticated:
+        topic.subscribers.add(request.user)
+        cache.delete("all_topics_cache")
+    return redirect("topics_view")
+
+
+@login_required
+def unsubscribe_view(request: HttpRequest, id: int) -> HttpResponse:
+    ''' Відписує користувача від теми, видаляє застарілий кеш та перенаправляє на сторінку тем. '''
+    topic = get_object_or_404(Topic, id=id)
+    if request.user.is_authenticated:
+        topic.subscribers.remove(request.user)
+        cache.delete("all_topics_cache")
+    return redirect("topics_view")
