bf6d5de5bb3f88552c4db187f4bd0cbc0387ad25
Eva <evacherkashyna9@gmail.com>
2025-12-28 16:04:16 +0200

    Refactor TopicForm to TopicsConfig with signals

--- topics/apps.py
+++ topics/apps.py
@@ -1,8 +1,10 @@
-from django import forms
-from .models import Topic
+from django.apps import AppConfig
 
 
-class TopicForm(forms.ModelForm):
-    class Meta:
-        model = Topic
-        fields = ['name']
+class TopicsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = "topics"
+
+    def ready(self):
+        from . import signals
+        signals.topic_changed.connect(signals.handle_topic_changed)
